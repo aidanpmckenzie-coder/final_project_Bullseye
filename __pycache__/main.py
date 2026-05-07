5557bd3189020f6d42bc2e37aa9aaa61e44b8029
McKenzie <aidan.mckenzie26@bcp.org>
2026-05-07 12:22:51 -0700

    I realized that the white ring on the board cannot be seen so I created an outline so the player can see it. I added the score function and using the MATH import i used the distance formula from the origin to where the point lands to determine what point the player gets for that shot. I updated the Info page to include what point you get for each ring. I made it so that player one's shot appears a different color on the screen than player two's shot so there is a visual representation of each persons shot on the screen. Player one is green and player two is hot pink. I also added code to update the points and score. I made code that alternates the players shooting but I am having trouble telling if they actually alternate due to the pen writing over itself. My main goal next is to create the different pens for different roles as stated in the previous summary and also adding code for when the game is overing and the results are released of which player won or if there was a tie. If I get through those ideas with time left, I will attempt to add sound to this game.

--- __pycache__/main.py
+++ __pycache__/main.py
@@ -10,10 +10,11 @@ import math
 from button import Button
 
 # Screen
+SCREEN_BACKGROUND_COLOR = "white"
 display_screen = "menu"
 screen = turtle.Screen()
 screen.setup(500, 500)
-screen.bgcolor("white")
+screen.bgcolor(SCREEN_BACKGROUND_COLOR)
 
 # Turtle Pen
 pen = turtle.Turtle()
@@ -26,7 +27,8 @@ player = 1 # player 1's turn
 arrows = 5 # each player gets 5 arrows
 wind_x = 0 # x coordinate to aim for
 wind_y = 0 # y coordinate to aim for
-
+player_one_score = 0
+player_two_score = 0
 
 
 # Main Menu
@@ -41,12 +43,14 @@ def draw_menu():
 
 
 # Archery Board Rings (Colors & radius)
+OUTLINE_RING_COLOR = "black"
 FIRST_RING_COLOR = "white"
 SECOND_RING_COLOR = "black"
 THIRD_RING_COLOR = "blue"
 FOURTH_RING_COLOR= "red"
 BULLSEYE_RING_COLOR = "yellow"
 
+OUTLINE_RING_RADIUS = 100.6
 FIRST_RING_RADIUS = 100
 SECOND_RING_RADIUS = 80
 THIRD_RING_RADIUS = 60
@@ -55,14 +59,13 @@ BULLSEYE_RING_RADIUS = 20
 
 # Arrow on Screen (shot taken)
 ARROW_SHOT_RADIUS = 8
-ARROW_SHOT = "green"
 
 # Archery Board (Game Screen)
 def draw_archery_board():
     pen.clear()
-    colors = [FIRST_RING_COLOR, SECOND_RING_COLOR, THIRD_RING_COLOR, FOURTH_RING_COLOR, BULLSEYE_RING_COLOR]
-    sizes = [FIRST_RING_RADIUS, SECOND_RING_RADIUS, THIRD_RING_RADIUS, FOURTH_RING_RADIUS, BULLSEYE_RING_RADIUS]
-    for i in range(5):
+    colors = [OUTLINE_RING_COLOR,FIRST_RING_COLOR, SECOND_RING_COLOR, THIRD_RING_COLOR, FOURTH_RING_COLOR, BULLSEYE_RING_COLOR]
+    sizes = [OUTLINE_RING_RADIUS, FIRST_RING_RADIUS, SECOND_RING_RADIUS, THIRD_RING_RADIUS, FOURTH_RING_RADIUS, BULLSEYE_RING_RADIUS]
+    for i in range(6):
         pen.goto(0, -sizes[i])
         pen.color(colors[i])
         pen.begin_fill()
@@ -81,7 +84,6 @@ def wind():
         wind_x = random.randint(-200,200)
         wind_y = random.randint(-200,200)
 
-
 # Game Text
 def draw_text():
     # Player (1 for now)
@@ -89,42 +91,125 @@ def draw_text():
     pen.write(f"Player: {player}",font=("Arial", 12))
 
     # Arrows (Maximum 5)
-    pen.goto(140, 220)
+    pen.goto(-220, 190)
     pen.write(f"Arrows: {arrows}",font=("Arial", 12))
 
     # Wind
     pen.goto(-80, -230)
     pen.write(f"Wind: ({wind_x}, {wind_y})",font=("Arial", 12))
 
+    # Scores
+    pen.goto(140, 220)
+    pen.write(f"P1: {player_one_score}", font=("Arial", 12))
+
+    pen.goto(140, 190)
+    pen.write(f"P2: {player_two_score}", font=("Arial", 12))
+
+# Score
+def score(x,y):
+    distance = math.dist((x, y), (0, 0))  # shot distance from bullseye center
+    # Hits Bullseye
+    if distance <= 20:
+        return 10
+    
+    # Hits Fourth Ring 
+    elif distance <= 40:
+        return 8
+    
+    # Hits Third Ring 
+    elif distance <= 60:
+        return 6
+    
+    # Hits Second Ring 
+    elif distance <= 80:
+        return 4
+    
+    # Hits First Ring 
+    elif distance <= 100:
+        return 2
+    
+    # Does not Hit Archery Board
+    else:
+        return 0
+
 
 # Information (Information Screen)
 def draw_info():
     pen.clear()
     # How to Play 
-    pen.goto(0, 125)
+    pen.goto(0, 190)
     pen.write("How to Play", align="center",font=("Arial", 20))
-    pen.goto(0, 80)
+    pen.goto(0, 160)
     pen.write("Welcome to Archery! The rules of the game are simple, rack up points by hitting a ring on the archery board.", align="center", font=("Arial", 14))
-    pen.goto(0,60)
+    pen.goto(0,140)
     pen.write("Hitting the board will get you points, and points increase the closer you are to the bullseye", align="center", font=("Arial", 14))
-    pen.goto(0,40)
+    pen.goto(0,120)
     pen.write("However, it is not as simple as just clicking on the bullseye, as wind will play a factor in this challenge.", align="center", font=("Arial", 14))
-    pen.goto(0,20)
+    pen.goto(0,100)
     pen.write("The game consists of 2 players, the player with the most points after each player runs out of arrows, wins!", align="center", font=("Arial", 14))
-    pen.goto(0,-20)
+    
+    # Score Distribution
+    pen.goto(0, 80)
+    pen.write("Bullseye: 10 Points", align="center",font=("Arial", 12))
+    pen.goto(0, 60)
+    pen.write("Red: 8 Points", align="center",font=("Arial", 12))
+    pen.goto(0, 40)
+    pen.write("Blue: 6 points", align="center",font=("Arial", 12))
+    pen.goto(0, 20)
+    pen.write("Black: 4 points", align="center",font=("Arial", 12))
+    pen.goto(0, 0)
+    pen.write("White: 2 points", align="center",font=("Arial", 12))
+    pen.goto(0, -20)
+    pen.write("Miss: 0 Points", align="center",font=("Arial", 12))
+
+
     # Start the Game
+    pen.goto(0,-70)
     pen.write("Click anywhere on the screen to begin!", align="center", font=("Arial", 14))
 
 
 # Play the Game (Shooting)
 def shoot(x,y):
+    global player, arrows, player_one_score, player_two_score
+
     # Shooting using the wind (corrects shot)
     x -= wind_x
     y -= wind_y
 
+    # Player arrow shots appear on screen in different colors to indicate which player shot where
+    if player == 1:
+        shot_color = "green"
+
+    else:
+        shot_color = "hot pink"
+
     # Draw shot on the screen
     pen.goto(x, y)
-    pen.dot(ARROW_SHOT_RADIUS, ARROW_SHOT)
+    pen.dot(ARROW_SHOT_RADIUS, shot_color)
+
+    # Updating Points
+    points = score(x,y)
+    if player == 1:
+        player_one_score += points
+    else:
+        player_two_score += points
+    
+    # Updating Game Stats after each shot
+    arrows -= 1
+
+    #alternate shots
+    if player == 1:
+        player = 2
+    else: 
+        player = 1
+
+    # Update 
+    wind()
+    draw_text()
+
+    # End the game
+    if arrows == 0:
+        print("Game Over!")
 
 
 # Clicking function
@@ -149,6 +234,8 @@ def click(x, y):
     elif display_screen == "info":
         display_screen = "game"
         draw_archery_board()
+        wind()
+        draw_text()
 
     # Begin the Game
     elif display_screen == "game":
